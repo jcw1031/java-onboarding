5480db51f662525efe0521b25ba42d98be45c73f
woopaca <jcw001031@gmail.com>
2022-12-26 13:26:10 +0900

    problem2 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,45 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        return decoder(cryptogram);
     }
+
+    public static String decoder(String cryptogram) {
+        List<Character> string = new ArrayList<>();
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            string.add(cryptogram.charAt(i));
+        }
+
+        while (true) {
+            if (string.size() < 2) {
+                break;
+            }
+            int i=1;
+            int size = string.size();
+            for (; i < size; i++) {
+                if (string.get(i).equals(string.get(i - 1))) {
+                    string.remove(i-1);
+                    string.remove(i-1);
+                    break;
+                }
+            }
+
+            if (i == size) {
+                break;
+            }
+        }
+
+        StringBuilder cryptogramBuilder = new StringBuilder();
+        for (Character c : string) {
+            cryptogramBuilder.append(c);
+        }
+        cryptogram = cryptogramBuilder.toString();
+        return cryptogram;
+    }
+
 }
