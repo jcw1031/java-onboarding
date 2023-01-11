6ba1dde57bf5baeec1fe0af4aa73c9abec9e10b5
woopaca <jcw001031@gmail.com>
2023-01-11 11:15:08 +0900

    Problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,30 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (Character.isLetter(ch)) {
+                ch = checkUpperOrLower(ch);
+            }
+            answer += ch;
+        }
+
         return answer;
+    }
+
+    public static char checkUpperOrLower(char ch) {
+        if (Character.isUpperCase(ch)) {
+            return upperCaseConvert(ch);
+        }
+        return lowerCaseConvert(ch);
+    }
+
+    public static char upperCaseConvert(char ch) {
+        return (char) ('A' + 'Z' - ch);
+    }
+
+    public static char lowerCaseConvert(char ch) {
+        return (char) ('a' + 'z' - ch);
     }
 }
